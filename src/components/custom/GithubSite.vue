21d88e2e49cbaf9a55285eb6d750b2ac8ddafde8
彭鞘 <878439818@qq.com>
2023-03-14 12:45:04 +0800

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="text-neutral-400">
     <span>Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
-      GitHub
+    <a href="http://chat.cqdnsai.com" target="_blank" class="text-blue-500">
+      CQDNSAI
     </a>
   </div>
 </template>
